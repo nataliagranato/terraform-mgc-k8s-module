acf5c063b37287b241a894e9770b1b09d19bb8f5
nataliagranato <nataliagranato@ufmg.br>
2024-07-24 21:40:42 -0300

    chore: Update Terraform configuration to create Kubernetes cluster and nodepool

--- main.tf
+++ main.tf
@@ -1,4 +1,4 @@
-# Create a Cluster with nodepool
+# Criando um cluster com um nodepool
 resource "mgc_kubernetes_cluster" "cluster_with_nodepool" {
   name                 = var.cluster_name
   enabled_bastion      = false
@@ -12,14 +12,14 @@ resource "mgc_kubernetes_cluster" "cluster_with_nodepool" {
   }]
 }
 
-# Set a timer to wait for the cluster to become active before creating node pools.
-# We acknowledge that this is not the optimal approach and are working to improve this behavior.
+# Tempo de espera para o cluster ficar ativo
+# Ajuste o tempo conforme necessário
 resource "time_sleep" "wait_15_minutes" {
   depends_on      = [mgc_kubernetes_cluster.cluster_with_nodepool]
   create_duration = var.timer_duration
 }
 
-# Create a Nodepool
+# Criando um nodepool
 resource "mgc_kubernetes_nodepool" "gp1_small" {
   depends_on = [time_sleep.wait_15_minutes] # Wait timer
   name       = "apis-2cpu-4gb-20gb"
@@ -30,4 +30,23 @@ resource "mgc_kubernetes_nodepool" "gp1_small" {
     min_replicas = 1
     max_replicas = 3
   }
+}
+
+# Timer para esperar o cluster ficar ativo
+resource "time_sleep" "wait_for_cluster" {
+  depends_on      = [mgc_kubernetes_cluster.cluster_with_nodepool]
+  create_duration = "10m" # Ajuste o tempo conforme necessário
+}
+
+# Pegar o kubeconfig do cluster usando o output do cluster_id
+data "mgc_kubernetes_cluster_kubeconfig" "cluster" {
+  depends_on = [time_sleep.wait_for_cluster]
+  cluster_id = mgc_kubernetes_cluster.cluster_with_nodepool.id
+}
+
+# Salvar o kubeconfig em um arquivo local
+resource "local_file" "kubeconfig" {
+  provider = local
+  content  = data.mgc_kubernetes_cluster_kubeconfig.cluster.kubeconfig
+  filename = "${path.module}/kubeconfig.yaml"
 }
